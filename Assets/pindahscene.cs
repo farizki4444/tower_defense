3470e9703556b4357c204c0cc671c106e551679d
farizki4444 <hermawanfarizki4444@gmail.com>
2021-09-19 21:18:49 +0700

    chapter 4

---
+++ Assets/pindahscene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pindahscene : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    public void PindahScene()
+{
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+   
+}  
+
